458d2ea86eefdbbd09f9af6ef6c049a94f479cc7
Jake Jaillet <jacob.jaillet@acd.ccac.edu>
2019-02-27 16:46:22 -0500

    tip calculator for bills

---
+++ bill.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 16:32:02 2019
+
+@author: jakej
+"""
+
+print("i am a tip calculator")
+bill=int(input("please enter the cost of the bill"))
+ten=int(bill*.10)
+fifteen=int(bill*.15)
+twenty=int(bill*.2)
+print("your ten percent tip is $", ten)
+print("your fifteen percent tip is $", fifteen)
+print("your twenty percent tip is $", twenty)
+
