70bb98aa9cb3781353bf2e78cb2bbb3832955981
Jake Jaillet <jacob.jaillet@acd.ccac.edu>
2019-02-27 16:48:08 -0500

    grade average calculator

---
+++ grade_calc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 16:21:59 2019
+
+@author: jakej
+"""
+
+print("hello, I will average 3 grades")
+grade1=int(input("Please enter Grade 1"))
+grade2=int(input("Please enter Grade 2"))
+grade3=int(input("Please enter Grade 3"))
+output=int((grade1+grade2+grade3)/3)
+print("the average grade is ", output, " Thank you")
