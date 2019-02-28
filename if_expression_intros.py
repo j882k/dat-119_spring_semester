5955123bd6808169ffc6d7aa005ef58b13835035
Jake Jaillet <jacob.jaillet@acd.ccac.edu>
2019-02-27 20:16:31 -0500

    labels days of week

---
+++ if_expression_intros.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 18:12:32 2019
+
+@author: jakej
+"""
+
+number=int(input("give a number between 1 and 7: "))
+if number == 1:
+    print("your day is Sunday")
+elif number == 2:
+    print("your day is Monday")
+elif number == 3:
+    print("your day is Tuesday")
+elif number == 4:
+    print("your day is Wednsday")
+elif number == 5:
+    print("your day is Thursday")
+elif number == 6:
+    print("your day is Friday")
+elif number == 7:
+    print("your day is Saturday")
+else:
+    print("The prompt asks for a number between 1 and 7, please refer back to original prompt")
+print("thank you")
