eb71dc9a6a4ec7f3cecc17e5ca16ecace4aca378
Jake Jaillet <jacob.jaillet@acd.ccac.edu>
2019-03-06 14:17:53 -0500

    leap year calculator

--- leap_year_calculator.py
+++ leap_year_calculator.py
@@ -6,10 +6,12 @@ Created on Wed Feb 27 20:16:39 2019
 """
 
 year=int(input("I am a leap year calculator, please enter a year (AD): "))
-if year % 4 == 0 and year >= 0:
+if year % 400 == 0:
     print("This year is a leap year, there was/will be 29 days in February")
-elif year % 4 > 0 and year >= 0:
+elif year % 4 > 0 or year % 100 == 0:    
     print("This year is not a leap year, there was/will be 28 days in February")
+elif year % 4 == 0 and year >= 0 and year % 100 > 0:
+    print("This year is a leap year, there was/will be 29 days in February")
 else:
     print("invalid year, please try again")
 print("Thank you")
