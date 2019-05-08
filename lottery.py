d654f9a83649e0a2ff90464ed7ad0593c53fbdbd
Jake Jaillet <jacob.jaillet@acd.ccac.edu>
2019-05-08 18:18:09 -0400

    not finished lotter sorry :(

---
+++ lottery.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 20:26:46 2019
+
+@author: jakej
+"""
+import random
+number1=random.randint(0,9)
+number2=random.randint(0,9)
+number3=random.randint(0,9)
+number4=random.randint(0,9)
+number5=random.randint(0,9)
+number6=random.randint(0,9)
+number7=random.randint(0,9)
+lottery=[number1,number2,number3,number4,number5,number6,number7]
+print("Today's lottery numbers go as follows:")
+for digit in lottery:
+    print(digit)
+
