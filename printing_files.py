376c93359149a8a7328109045f2d68ffc2301068
Jake Jaillet <jacob.jaillet@acd.ccac.edu>
2019-05-08 19:13:34 -0400

    just prints to an independent file

---
+++ printing_files.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 19:01:28 2019
+
+@author: jakej
+"""
+steps=open('step_averages.txt','w')
+data=open('steps.txt','r')
+boom = []
+boom = (data.readlines.rstrip('\n'))
+print(boom[0:30])
